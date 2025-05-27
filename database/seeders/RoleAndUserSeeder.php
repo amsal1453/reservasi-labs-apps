1fadd264d19c2f33924a7e841f2a9d9d3d9e1447
amsal1453 <kunamsaldasilva@gmail.com>
2025-05-27 13:35:41 +0700

    fungsi login sudah selesai pada admin

--- database/seeders/RoleAndUserSeeder.php
+++ database/seeders/RoleAndUserSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class RoleAndUserSeeder extends Seeder
 {
@@ -12,6 +15,42 @@ class RoleAndUserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //// 1. Buat Role
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        $lecturerRole = Role::firstOrCreate(['name' => 'lecturer']);
+        $studentRole = Role::firstOrCreate(['name' => 'student']);
+
+        // 2. Buat Admin
+        $admin = User::firstOrCreate(
+            ['email' => 'admin@kampus.ac.id'],
+            [
+                'name' => 'Admin User',
+                'nim_nip' => 'admin001',
+                'password' => Hash::make('password123'),
+            ]
+        );
+        $admin->assignRole($adminRole);
+
+        // 3. Buat Dosen
+        $lecturer = User::firstOrCreate(
+            ['email' => 'dosen@kampus.ac.id'],
+            [
+                'name' => 'Lecturer User',
+                'nim_nip' => 'nip123456',
+                'password' => Hash::make('password123'),
+            ]
+        );
+        $lecturer->assignRole($lecturerRole);
+
+        // 4. Buat Mahasiswa
+        $student = User::firstOrCreate(
+            ['email' => 'mahasiswa@student.kampus.ac.id'],
+            [
+                'name' => 'Student User',
+                'nim_nip' => 'nim2023001',
+                'password' => Hash::make('password123'),
+            ]
+        );
+        $student->assignRole($studentRole);
     }
 }
